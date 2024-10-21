81b9d5f9fbf308cfaf809a7ae5ebdd0dd868b905
yatharth tripathi <156705390+yathudon@users.noreply.github.com>
2024-10-21 17:46:05 +0530

    wallet functions completed

--- src/SolanaWallet.jsx
+++ src/SolanaWallet.jsx
@@ -1,27 +1,24 @@
-import { useState } from "react";
+import { useState } from "react"
 import { mnemonicToSeed } from "bip39";
 import { derivePath } from "ed25519-hd-key";
-import { Keypair, Keypair } from "@solana/web3.js";
-import nacl from "tweetnacl";
-
+import { Keypair } from "@solana/web3.js";
+import nacl from "tweetnacl"
 
 export function SolanaWallet({ mnemonic }) {
-    const [currentIndex,setCurrentIndex] = useState{0};
-    const [publicKeys ,setPublicKeys] = useState([]);
-
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [publicKeys, setPublicKeys] = useState([]);
 
     return <div>
-        <button
-            onClick = {async function () {
-                const seed = await mnemonicToSeed(mnemonic);
-                const path = `m/44'/501'/$(currentIndex)'/0'`;
-                const derivedSeed = derivePath(path,seed.toString("hex")).key;
-                const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
-                const  Keypair = Keypair.fromSecretKey(secret);
-                setCurrentIndex(currentIndex + 1);
-                setPublicKeys([...publicKeys,Keypair.publicKey])
-                
-            }}>   Add SOL Wallet 
+        <button onClick={async function() {
+            const seed = await mnemonicToSeed(mnemonic);
+            const path = `m/44'/501'/${currentIndex}'/0'`;
+            const derivedSeed = derivePath(path, seed.toString("hex")).key;
+            const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+            const keypair = Keypair.fromSecretKey(secret);
+            setCurrentIndex(currentIndex + 1);
+            setPublicKeys([...publicKeys, keypair.publicKey]);
+        }}>
+            Add SOL wallet
         </button>
         {publicKeys.map(p => <div>
             {p.toBase58()}
