5f421e1cef12ccc01fb453ff549406f3964ec9c7
yatharth tripathi <156705390+yathudon@users.noreply.github.com>
2024-10-21 22:15:23 +0530

    UI fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import './App.css'
-import { generateMnemonic } from "bip39";
+import { useState } from 'react';
+import './App.css';
+import { generateMnemonic } from 'bip39';
 import { SolanaWallet } from './SolanaWallet';
 import { EthWallet } from './EthWallet';
 
@@ -8,19 +8,30 @@ function App() {
   const [mnemonic, setMnemonic] = useState("");
 
   return (
-    <>
-      <input type="text" value={mnemonic}></input>
-      <button onClick={async function() {
+    <div className="container">
+      <h1>Batuaa</h1>
+      <h2 className="wallet-name">Wallet For Solana and Ethereum</h2>
+
+      <input 
+        type="text" 
+        value={mnemonic} 
+        placeholder="Your seed phrase will appear here" 
+        readOnly 
+      />
+      
+      <button onClick={async () => {
         const mn = generateMnemonic();
-        setMnemonic(mn)
+        setMnemonic(mn);
       }}>
         Create Seed Phrase
       </button>
 
-      {mnemonic && <SolanaWallet mnemonic={mnemonic} />}
-      {mnemonic && <EthWallet mnemonic={mnemonic} />}
-    </>
-  )
+      <div className="wallets">
+        {mnemonic && <SolanaWallet mnemonic={mnemonic} />}
+        {mnemonic && <EthWallet mnemonic={mnemonic} />}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
